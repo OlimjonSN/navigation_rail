1b34ecc96ec3691acabe5d6a555bc3621ceaf091
Olimjon <olimjonsn230@gmail.com>
2022-12-19 14:46:42 +0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,9 @@ class _RailState extends State<Rail> {
       body: Row(
         children: <Widget>[
           NavigationRail(
+            groupAlignment: 0,
+            labelType: NavigationRailLabelType.selected,
+            backgroundColor: Colors.orange,
             selectedIndex: _selectedIndex,
             onDestinationSelected: (int index) {
               setState(() {
@@ -44,22 +47,23 @@ class _RailState extends State<Rail> {
               NavigationRailDestination(
                 icon: Icon(Icons.bookmark_border),
                 selectedIcon: Icon(Icons.book),
-                label: Text('Second'),
+                label: Text('Second',style: TextStyle(color: Colors.white),),
+                
               ),
               NavigationRailDestination(
                 icon: Icon(Icons.star_border),
-                selectedIcon: Icon(Icons.star),
+                selectedIcon: Icon(Icons.star,color: Colors.white,),
                 label: Text('Third'),
               ),
             ],
           ),
-          const VerticalDivider(thickness: 1, width: 1),
+          const VerticalDivider(thickness: 2, width: 5,color: Colors.blue,),
           // This is the main content.
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text("Olimjon")
+                Text("this is navigation rail")
               ],
             ),
           ),
